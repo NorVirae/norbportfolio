0dc200731f8aa176697338e48bec7fd086d42a28
Fischela <fischelaltd@gmail.com>
2021-11-04 12:24:34 +0100

    Uploading my portfolio

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+
+const server = express()
+
+const port = process.env.port||8000
+
+// server.get("/", (req, res)=>{
+//     console.log("8000 was accessed")
+//     res.send("connection was successful")
+// })
+
+
+server.use(express.static(__dirname+"/build"))
+server.get("/", (req, res)=>{
+    res.sendFile(__dirname+"/build/"+"index.html")
+})
+
+server.get("*", (req, res)=>{
+    res.sendFile(__dirname+"/build/"+"index.html")
+})
+
+server.listen(port, (dats)=>{
+    console.log("Server connected successfully at port "+port)
+})
+
